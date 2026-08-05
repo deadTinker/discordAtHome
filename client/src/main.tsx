8ba265da237b32375f5f1de6ed3ca9aabb92e4aa
Divyansh <Divyanshgautam7115@gmail.com>
2026-08-05 14:55:42 +0530

    Removed unused StrictMode import

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import { BrowserRouter } from "react-router-dom";
@@ -7,9 +6,7 @@ import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  // </StrictMode>, 
 )
