0e4498909a4c857c6afe81974d599f607da963d8
Divyansh <Divyanshgautam7115@gmail.com>
2026-08-05 14:31:45 +0530

    Configured environment variables for deployment

--- client/src/socket/socket.ts
+++ client/src/socket/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 
-const socket = io("http://localhost:3000");
+const socket = io(import.meta.env.VITE_SERVER_URL);
 
 socket.on("connect", () =>{
     console.log("Connected!");
