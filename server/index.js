4f77271f5188372e1c43723cdd54f76c10a0a287
Divyansh <Divyanshgautam7115@gmail.com>
2026-08-05 15:13:27 +0530

    Fixed CORS for deployed frontend

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const io = new Server(server, {
     cors: { 
         origin: [
             "http://localhost:5173",
-            "https://cordisk.vercel.app/"
+            "https://cordisk.vercel.app"
         ],
         methods: ["GET", "POST"]
     }
